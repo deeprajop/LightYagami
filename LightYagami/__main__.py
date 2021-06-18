09dcb6de8237f44affdc4a32928dc8f391165b93
deeprajop <84038103+deeprajop@users.noreply.github.com>
2021-06-18 19:22:04 +0530

    Update __main__.py

--- LightYagami/__main__.py
+++ LightYagami/__main__.py
@@ -540,7 +540,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "❆ Zer0Byte ❆")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!")
